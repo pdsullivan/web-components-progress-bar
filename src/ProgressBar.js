1350f28829f92dffb435590eaf0067f69f6391da
traktorbeam <patrick@pdsullivan.com>
2017-07-24 18:10:51 -0700

    click progress bar component to remove it

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -5,36 +5,45 @@ export default class ProgressBar extends HTMLElement {
         this.progress = 0;
         this.createProgressBar();
         this.startProgress();
+        // console.info(`START: ${new Date()} ${new Date().getMilliseconds()}`);
     }
 
     createProgressBar() {
         this.progressBarContainer = document.createElement('div');
         this.progressBarContainer.style.border = '1px solid grey';
+        this.progressBarContainer.style.width = '250px';
         this.progressBarContainer.style.height = '24px';
         this.progressBarContainer.style.margin = '24px';
+        this.progressBarContainer.addEventListener('click', () => this.removeSelf());
 
         this.progressBar = document.createElement('div');
         this.progressBar.setAttribute('class', 'progress-bar-div');
         this.progressBar.style.height = '24px';
         this.progressBar.style.backgroundColor = 'grey';
+        this.setProgress(0);
 
         this.progressBarContainer.appendChild(this.progressBar);
-
         this.shadow.appendChild(this.progressBarContainer);
     }
 
+    removeSelf() {
+        this.shadow.removeChild(this.progressBarContainer);
+    }
+
     startProgress() {
-        if (this.progress === 100) return;
+        if (this.progress === 100) {
+            // console.info(`END: ${new Date()} ${new Date().getMilliseconds()}`);
+            return;
+        }
         setTimeout(() => {
             this.setProgress(this.increment())
             this.startProgress();
-        }, 10);
+        }, (3 * 60000) / 10000);
     }
 
     setProgress(progress) {
         this.progress = progress;
         this.progressBar.style.width = `${this.progress}%`;
-        console.log(this.progress)
     }
 
     increment() {
